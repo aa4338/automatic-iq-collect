51b2cdcdf6fd57c00226fd3c67469c80a4e0c951
Abhishek Adhikari <aa4338@drexel.edu>
2020-06-07 21:42:13 -0500

    .

--- run_radio.bash
+++ run_radio.bash
@@ -18,31 +18,17 @@ echo $gn_rx_ip
 # modulation_tx = bpsk
 # modulation_rx = bpsk
 
-# # connect ethernet, set frequency, run radio
-# run_rx="sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'ifconfig eth1 192.168.10.1;\
-# cd dragonradio;./dragonradio python/standalone-radio.py -i 1 -f 1.312e9 --log-iq -m bpsk'"
 
-# # do the same with tx 
-# run_tx="sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'ifconfig eth1 192.168.10.1;ls>text.txt;\
-# cd dragonradio;./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk'"
+# run the rx radio first, in a new tmux session that is kept running
+tmux new-session -d -A -s remote_rx
+tmux send-keys -t remote_rx "sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'ifconfig eth1 192.168.10.1;\
+cd dragonradio;./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk'" C-m
+
+# run the tx radio now
+tmux new-session -d -A -s remote_tx
+tmux send-keys -t remote_tx "sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'ifconfig eth1 192.168.10.1;\
+cd dragonradio;./dragonradio python/standalone-radio.py -i 1 -f 1.312e9 --log-iq -m bpsk'" C-m
 
 
-# tmux new -s "remote_rx" -d
-# tmux send-keys -t "remote_rx" "sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'ifconfig eth1 192.168.10.1;\
-# cd dragonradio;./dragonradio python/standalone-radio.py -i 1 -f 1.312e9 --log-iq -m bpsk'" C-m
-#tmux kill-ses -t remote
-
-# tmux new -s "remote_tx" -d
-# tmux send-keys -t "remote_tx" "sshpass -p 'kapilrocks' ssh root@$gn_tx_ip"
-# tmux send-keys -t "remote_tx" "ls>text.txt" C-m
-# tmux attach -t "remote_tx" -d
-# # tmux send-keys -t "remote_tx" "sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'ifconfig eth1 192.168.10.1;ls>text.txt;\
-# # cd dragonradio;./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk'" C-m
-
-# tmux kill-ses -t remote_rx
-# tmux kill-ses -t remote_tx
-
-tmux new-session -d -A -s encode
-tmux send-keys -t encode "sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'ifconfig eth1 192.168.10.1;ls>text.txt'" C-m
 #tmux kill-ses -t encode
 
