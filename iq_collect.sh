d7238f72ad040dc0da97a242fca09f78ac541527
Abhishek Adhikari <aa4338@drexel.edu>
2020-06-08 01:27:06 -0500

    .

--- iq_collect.sh
+++ iq_collect.sh
@@ -6,6 +6,7 @@ if test "$#" -lt 3; then
     echo ""
     echo "Use: ./iq_collect <modulation> <TX grid node #> <RX grid node #>"
     echo ""
+    sleep 1.5
     echo "Available Modulations:"
     echo "==============="
     echo "bpsk"
