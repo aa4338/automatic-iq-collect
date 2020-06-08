011f010d3c98f199c9bafd57d0ced72b19244b43
Abhishek Adhikari <aa4338@drexel.edu>
2020-06-07 23:42:03 -0500

    .

--- build_dragonradio.bash
+++ build_dragonradio.bash
@@ -21,13 +21,34 @@ echo $gn_rx_ip
 # sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'cd dragonradio;yes | ./build.sh -j5' 
 # sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'cd dragonradio;yes | ./build.sh -j5'       
 
-#tmux
+# RX first
 tmux new -d -s start_rx
 tmux send-keys -t start_rx "sshpass -p 'kapilrocks' ssh root@$gn_rx_ip" C-m
 tmux send-keys -t start_rx "ifconfig eth1 192.168.10.1" C-m
 tmux send-keys -t start_rx "cd dragonradio" C-m
 tmux send-keys -t start_rx "./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk" C-m
-tmux attach -t start_rx
+
+# TX
+tmux new -d -s start_tx
+tmux send-keys -t start_tx "sshpass -p 'kapilrocks' ssh root@$gn_tx_ip" C-m
+tmux send-keys -t start_tx "ifconfig eth1 192.168.10.1" C-m
+tmux send-keys -t start_tx "cd dragonradio" C-m
+tmux send-keys -t start_tx "./dragonradio python/standalone-radio.py -i 1 -f 1.312e9 --log-iq -m bpsk" C-m
+
+# Iperf RX
+tmux new -d -s iperf_rx
+tmux send-keys -t iperf_rx "sshpass -p 'kapilrocks' ssh root@$gn_rx_ip" C-m
+tmux send-keys -t iperf_rx "iperf -s -u -i 1" C-m
+
+# Iperf TX
+tmux new -d -s iperf_tx
+tmux send-keys -t iperf_tx "sshpass -p 'kapilrocks' ssh root@$gn_rx_ip" C-m
+tmux send-keys -t iperf_tx "iperf -s -u -i 1" C-m
+iperf -c 10.10.10.1 -u -i 1 -b 200k -t 10
+
+tmux attach -t iperf_rx
+
+
 # sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'ifconfig eth1 192.168.10.1;\
 # cd dragonradio;./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk' 
 
