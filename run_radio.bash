fb91f6a68f62fdc0c7365be191cc065af33191e8
Abhishek Adhikari <aa4338@drexel.edu>
2020-06-07 20:53:39 -0500

    .

--- run_radio.bash
+++ run_radio.bash
@@ -14,17 +14,17 @@ gn_rx_ip=$(gridcli -gn grid$gn_rx -ip)
 echo $gn_tx_ip
 echo $gn_rx_ip
 
-# set the modulations you want
-modulation_tx = bpsk
-modulation_rx = bpsk
+# # set the modulations you want
+# modulation_tx = bpsk
+# modulation_rx = bpsk
 
 # connect ethernet, set frequency, run radio
-run_rx=sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'ifconfig eth1 192.168.10.1;\
-cd dragonradio;./dragonradio python/standalone-radio.py -i 1 -f 1.312e9 --log-iq -m bpsk' 
+run_rx="sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'ifconfig eth1 192.168.10.1;\
+cd dragonradio;./dragonradio python/standalone-radio.py -i 1 -f 1.312e9 --log-iq -m bpsk'"
 
 # do the same with tx 
-run_tx=sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'ifconfig eth1 192.168.10.1;\
-cd dragonradio;./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk' 
+run_tx="sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'ifconfig eth1 192.168.10.1;\
+cd dragonradio;./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk'"
 
 
 tmux new -s "remote" -d
