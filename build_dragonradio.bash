6acc9cad2df8b44e2efec56c4f17a4cbf7ebe404
Abhishek Adhikari <aa4338@drexel.edu>
2020-06-07 22:09:21 -0500

    .

--- build_dragonradio.bash
+++ build_dragonradio.bash
@@ -4,19 +4,33 @@
 
 
 # defining grid nodes
-gn_tx=grid10
-gn_rx=grid12
+gn_tx=10
+gn_rx=12
 
 # starting up respective containers
-gridcli -gn $gn_tx --start -i channel-data-radio-20191003
-gridcli -gn $gn_rx --start -i channel-data-radio-20191003
+gridcli -gn grid$gn_tx --start -i channel-data-radio-20191003
+gridcli -gn grid$gn_rx --start -i channel-data-radio-20191003
 
 # finding the ip of them
-gn_tx_ip=$(gridcli -gn $gn_tx -ip)
-gn_rx_ip=$(gridcli -gn $gn_rx -ip)
+gn_tx_ip=$(gridcli -gn grid$gn_tx -ip)
+gn_rx_ip=$(gridcli -gn grid$gn_rx -ip)
 
 echo $gn_tx_ip
 echo $gn_rx_ip
 
-sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'cd dragonradio;yes | ./build.sh' 
-sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'cd dragonradio;yes | ./build.sh'        
+# sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'cd dragonradio;yes | ./build.sh' 
+# sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'cd dragonradio;yes | ./build.sh'        
+
+# run the rx radio first, in a new tmux session that is kept running
+tmux new-session -d -A -s remote_rx
+tmux send-keys -t remote_rx "sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'ifconfig eth1 192.168.10.1;\
+cd dragonradio;./dragonradio python/standalone-radio.py -i 2 -f 1.312e9 --log-iq -m bpsk'" C-m
+
+# run the tx radio now
+tmux new-session -d -A -s remote_tx
+tmux send-keys -t remote_tx "sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'ifconfig eth1 192.168.10.1;\
+cd dragonradio;./dragonradio python/standalone-radio.py -i 1 -f 1.312e9 --log-iq -m bpsk'" C-m
+
+
+sshpass -p 'kapilrocks' ssh root@$gn_rx_ip 'iperf -s -u -i 1'   
+sshpass -p 'kapilrocks' ssh root@$gn_tx_ip 'iperf -c 10.10.10.1 -u -i 1 -b 200k -t 10' 
