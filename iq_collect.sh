1f83009bcc9daf7f7de4775e808fc22e9105c073
Abhishek Adhikari <aa4338@drexel.edu>
2020-06-20 13:00:26 -0500

    .

--- iq_collect.sh
+++ iq_collect.sh
@@ -83,4 +83,11 @@ mv radio.h5 iq_collect_$modulation.h5
 end message
 echo "Your requested $modulation file has been downloaded."
 
+read -s -p "Plot? (yes/no) " response
+if response=yes then;
+    tmux send-keys -t 2 'cd tools' C-m \; \
+    tmux send-keys -t 2 'source env/bin/activate' C-m \; \
+    tmux send-keys -t 2 './drgui.py ../logs/node-001/radio.h5 --rx 1' C-m \; 
+fi
+
 fi
