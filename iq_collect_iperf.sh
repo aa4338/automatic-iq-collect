c8fdba0338e2e1bc13f92d26960ed7c015e92af6
Abhishek Adhikari <aa4338@drexel.edu>
2020-08-02 16:16:14 -0500

    .

--- iq_collect_iperf.sh
+++ iq_collect_iperf.sh
@@ -85,11 +85,11 @@ tmux new-session \; \
     send-keys -t 2 'ls' C-m \; \
     send-keys -t 2 'ifconfig eth1 192.168.10.1' C-m \; \
     send-keys -t 2 'cd dragonradio' C-m \; \
-    send-keys -t 2 "timeout 20 sudo ./dragonradio python/ecet680-radio.py -i 2 -f 1.3${gn_rx}e9 -m $modulation -l logs --log-iq --log-snapshot --snapshot-duration 5 --snapshot-period 5 -m $modulation" C-m '' \; \
+    send-keys -t 2 "timeout 15 sudo ./dragonradio python/ecet680-radio.py -i 2 -f 1.3${gn_rx}e9 -m $modulation -l logs --log-iq --log-snapshot --snapshot-duration 5 --snapshot-period 5 -m $modulation" C-m '' \; \
     send-keys -t 0 "sshpass -p 'kapilrocks' ssh -X root@$gn_tx_ip" C-m '' \; \
     send-keys -t 0 'ifconfig eth1 192.168.10.1' C-m \; \
     send-keys -t 0 'cd dragonradio' C-m \; \
-    send-keys -t 0 "timeout 30 sudo ./dragonradio python/ecet680-radio.py -i 1 -f 1.3${gn_rx}e9 -m $modulation" C-m '' \; \
+    send-keys -t 0 "timeout 15 sudo ./dragonradio python/ecet680-radio.py -i 1 -f 1.3${gn_rx}e9 -m $modulation" C-m '' \; \
     send-keys -t 3 "sshpass -p 'kapilrocks' ssh -X root@$gn_rx_ip" C-m '' \; \
     send-keys -t 3 'sleep 3 && sudo iperf -s -u -i 1' C-m \; \
     send-keys -t 1 "sshpass -p 'kapilrocks' ssh -X root@$gn_tx_ip" C-m '' \; \
